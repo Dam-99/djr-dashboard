096522e2e9d401939d5ffec63a09ee1d9ac3c3d9
Damiano Mason <damiano.mason@secomind.com>
2025-06-18 16:03:33 +0200

    fix: simplified issue migrations and schemas
    Signed-off-by: Damiano Mason <damiano.mason@secomind.com>

--- lib/djr_dashboard/chapter.ex
+++ lib/djr_dashboard/chapter.ex
@@ -5,8 +5,14 @@ defmodule DjrDashboard.Chapter do
   schema "chapters" do
     field :chap_number, :integer
     field :manga_id, Ecto.UUID
-    field :issue_number, :string
-    field :issue_year, :integer
+    field :issue_id, Ecto.UUID
+    # field :issue_number, :string
+    # field :issue_year, :integer
+
+    # this is what i'd like to do instead...
+    # belongs_to :issue, Issue, type: Ecto.UUID
+    # belongs_to :issue_num, Issue, foreign_key: :issue_number, type: :string
+    # belongs_to :issue_yy, Issue, foreign_key: :issue_year, type: :integer
 
     timestamps(type: :utc_datetime)
   end
--- lib/djr_dashboard/issue.ex
+++ lib/djr_dashboard/issue.ex
@@ -5,9 +5,11 @@ defmodule DjrDashboard.Issue do
   # @primary_key false
   # would like to use composite primary key, but it's a bit of a pain:
   # https://elixirforum.com/t/does-ecto-supports-composite-foreign-keys/2466/6
+  @primary_key {:id, Ecto.UUID, autogenerate: true}
+  @foreign_key_type Ecto.UUID
   schema "issues" do
-    field :number, :string, primary_key: true
-    field :year, :integer, primary_key: true
+    field :number, :string
+    field :year, :integer
     many_to_many :mangas, DjrDashboard.Manga, join_through: DjrDashboard.Chapter
 
     timestamps(type: :utc_datetime)
@@ -16,7 +18,7 @@ defmodule DjrDashboard.Issue do
   @doc false
   def changeset(issue, attrs) do
     issue
-    |> cast(attrs, [:number, :year])
+    |> cast(attrs, [:id, :number, :year])
     |> validate_required([:number, :year])
   end
 end
