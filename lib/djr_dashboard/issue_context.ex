67d215e499dd2da5bb65d9ed8bc523afa526e033
Damiano Mason <damiano.mason@secomind.com>
2025-06-18 16:05:22 +0200

    feat: functioning api issue routes
    Signed-off-by: Damiano Mason <damiano.mason@secomind.com>

---
+++ lib/djr_dashboard/issue_context.ex
@@ -0,0 +1,39 @@
+defmodule DjrDashboard.IssueContext do
+  @moduledoc """
+  The Issue context.
+  """
+
+  import Ecto.Query, warn: false
+  alias DjrDashboard.Repo
+
+  alias DjrDashboard.Issue
+
+  @doc """
+  returns the list of reviewed issues
+  """
+  def list_issues() do
+    # Repo.all(Issue)
+    Repo.all(from(i in Issue, select: [i.id, i.year, i.number]))
+  end
+
+  def get_issue({year, number}) do
+    {:ok, %{num_rows: 1, rows: [issue]}} =
+      Repo.one(Issue, [year: year, number: number])
+    issue
+  end
+
+  def get_issue(id) do
+    Repo.get(Issue, id)
+  end
+
+  def create_issue({year, number}) do
+    %Issue{}
+    |> Issue.changeset(%{year: year, number: number})
+    |> Repo.insert(returning: [:year, :number])
+  end
+
+  def delete_issue(id_or_yrnr) do
+    issue = get_issue(id_or_yrnr)
+    Repo.delete(issue)
+  end
+end
