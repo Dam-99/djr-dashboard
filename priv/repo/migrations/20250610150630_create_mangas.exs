a80d9287269e29883f6c7b66d37ee90945e2dd23
Damiano Mason <damiano.mason@secomind.com>
2025-06-23 17:38:49 +0200

    fix: missed change for create_manga migration in 0260541

--- priv/repo/migrations/20250610150630_create_mangas.exs
+++ priv/repo/migrations/20250610150630_create_mangas.exs
@@ -2,8 +2,7 @@ defmodule DjrDashboard.Repo.Migrations.CreateMangas do
   use Ecto.Migration
 
   def change do
-    create table(:mangas, primary_key: false) do
-      add :id, :uuid, primary_key: true
+    create table(:mangas) do
       add :title, :string, null: false
 
       timestamps(type: :utc_datetime)
